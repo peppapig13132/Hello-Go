120b4f00f35c4b4e7cd0db89bd3980152a6b0dad
peppapig13132 <peppapig13132@outlook.com>
2024-07-17 06:24:25 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🎉Hello, Go
+
+```
+go run hello.go
+```
+
+```
+Hello, World
+Привет, мир
+你好世界
+Hallo Welt
+```
---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// En
+	fmt.Println("Hello, World")
+
+	// Ru
+	fmt.Println("Привет, мир")
+
+	// Cn
+	fmt.Println("你好世界")
+
+	// De
+	fmt.Println("Hallo Welt")
+}
